f927f8e3633dde3d8b1c76e5c1d7147bda23b8d1
christineclr <167209490+christineclr@users.noreply.github.com>
2024-04-17 08:05:53 +0700

    Add files via upload

---
+++ views/about_tampil.php
@@ -0,0 +1,41 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> About</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="deskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <p align="center"><img src="img/FEBUNDIP.png"></img></p>
+                            <label class="col-sm-12 control-label"><center><strong>SISTEM INFORMASI PERPUSTAKAAN <br> Fakultas Ekonomika dan Bisnis</strong></center></label>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            Sistem Informasi Perpustakaan Fakultas Ekonomika dan Bisnis
+                            <br>
+                            <br>
+                            Universitas Diponegoro
+                            <br>
+                            Nama : Christine Claradema Simanjuntak
+                            <br>
+                            NIM  : 12030122140214
+                            <br>
+                            Kelas: Pengkodean dan Pemrograman F
+                            <br>
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
---
+++ views/kontak_tampil.php
@@ -0,0 +1,38 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> Kontak</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="deskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <label class="col-sm-12 control-label"><center><strong>PERPUSTAKAAN FAKULTAS EKONOMIKA DAN BISNIS</strong></center></label>
+                            <p align="center">
+                              Jalan Prof. Moeljono S. Trastotenojo, Tembalang, Kec. Tembalang<br>
+                              Semarang, Jawa Tengah, Kode Pos : 50275
+                            </p>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            Telp. 02476486841<br>
+                            Email : feb.undip@gmail.com<br>
+                            Fax : 1342657<br>
+                            Website : febundip.ac.id<br>
+                            <br>
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
